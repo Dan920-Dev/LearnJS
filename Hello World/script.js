a930e85004dac41daa36cccfe27e7eae619857d8
Dan920-Dev <00138218@uca.edu.sv>
2022-12-03 18:05:38 -0600

    Add change color example

---
+++ Hello World/script.js
@@ -0,0 +1,11 @@
+// Muestra una alerta con ese mensaje
+alert('Hello This is a Great Place to learn');
+
+// Se accede al texto mediante el ID
+var text = document.getElementById('text');
+
+// Cambia el color del text a rojo
+function changeColor() {
+    text.style.color = "red";
+}
+
